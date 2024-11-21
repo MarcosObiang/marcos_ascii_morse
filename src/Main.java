838e357d993e28d636e9c7ded8ab6bd94da46d59
JorgeOrtegaCorral <jorge.corral@estudiante.epsum.school>
2024-11-21 10:42:28 +0100

    arreglando fallos

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scaner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         MorseAscii morseAscii = new MorseAscii();
         AsciiMorse asciiMorse = new AsciiMorse();
 
         while (true){
-            System.out.println("Elije un numero para traducir: Ascii --> Morese (1) / Morse --> Ascii (2):");
-            int numeroTraduccion = scaner.nextInt();
+            System.out.println("Elije un numero para traducir: Ascii --> Morese (1) / Morse --> Ascii (2), para salir (3):");
+            int numeroTraduccion = scanner.nextInt();
+            scanner.nextLine();
 
             if(numeroTraduccion == 1){
                 System.out.println("Introduce tu frase en Ascii: ");
-                String morseSentence = scaner.nextLine();
+                String asciiSentence = scanner.nextLine();
 
-                String asciiText = MorseAscii.traducir(morseSentence);
+                String morseText = morseAscii.traducir(asciiSentence);
 
-                System.out.println("Translated Morse (text): " + asciiText);
-                scaner.close();
+                System.out.println("Translated Morse (text): " + morseText);
             } else if (numeroTraduccion == 2) {
                 System.out.println("Introduce tu frase en Morse: ");
-                String morseSentence = scaner.nextLine();
+                String morseSentence = scanner.nextLine();
 
                 String asciiText = asciiMorse.traducir(morseSentence);
 
                 System.out.println("Translated Ascii (text): " + asciiText);
-                scaner.close();
+            } else if (numeroTraduccion == 3){
+                System.out.println("Saliendo...");
+                break;
             } else {
                 System.out.println("Introduce un numero valido");
             }
         }
-
-
+        scanner.close();
     }
 }
